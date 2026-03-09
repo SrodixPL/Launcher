6bb4f8e83c4d9b77cfd6bf0c9510f0653d168b51
Srodix <srodagameyt1@gmail.com>
2026-03-09 02:10:41 +0100

    Initial commit

---
+++ Core/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Core
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitAsync();
+        }
+
+        async void InitAsync()
+        {
+            await webView.EnsureCoreWebView2Async();
+            webView.Source = new Uri("http://localhost:5173");
+        }
+    }
+}
